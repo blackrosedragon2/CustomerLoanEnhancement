f09876a4ef88c76c6bc75d699332c5d1386086f1
blackrosedragon2 <helloharshit68@gmail.com>
2021-07-05 21:02:25 +0530

    Add Enhancement Project

---
+++ authorization-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user(id, name, password) values (1, 'harshit', 'admin');
+
+
+
+
+
+insert into customer(id,name,password) values (100011,'vinay','vinay');
+insert into customer(id,name,password) values (100012,'cogni','cogni');
